7047d7c3b9fec39b1540f80d80393a2cf81da9b5
Devansh M <devansh.m100@gmail.com>
2026-03-23 11:29:03 +0530

    feat: enhance graph.py with additional logic and validate via PyCharm debugger
    - Added new code updates to graph.py for improved LLM interaction flow
    - Refined prompt handling and response generation logic
    - Performed step-by-step debugging using PyCharm's inbuilt debugger
    - Verified correct execution and response output

--- agent/graph.py
+++ agent/graph.py
@@ -1,18 +1,33 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from langchain_groq import ChatGroq
 
-llm = ChatGroq(model = "openai/gpt-oss-120b")
+llm = ChatGroq(model="openai/gpt-oss-120b")
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-class Schema(BaseModel):
-    price: float
-    eps: float
+user_prompt = "create a simple calculator web application"
 
-resp = llm.invoke("Extract Price and EPS from this report: "
-                  "NVIDIA reported quarterly EPS of 2.3 and "
-                  "their current price is $100.")
+prompt = f"""
+You are the PLANNER agent. Convert the user prompt into a COMPLETE engineering project plan
+
+User request: {user_prompt}
+"""
+
+class File(BaseModel):
+    path: str = Field(description="path to file to be created or modified")
+    purpose: str = Field(description="purpose of the file, e.g. 'main application logic', 'data processing module', etc.")
+
+class Plan(BaseModel):
+    name: str = Field(description="the name of the app to be built")
+    description: str = Field(description="the description of the app to be built")
+    techstack: str = Field(description="the techstack to be used for the app")
+    features: list[str] = Field(
+        description="the list of features to be used for the app, e.g. 'user authentication', 'data_visualization', etc.")
+    files: list[File] = Field(description="a list of files to be created, each with a 'path' and 'purpose'")
+
+resp = llm.with_structured_output(Plan).invoke(prompt)
 
 print(resp)
